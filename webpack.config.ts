cc32562b841776b6de6b3319b0cc5eca93305ca6
Keiji Miyazaki <miyah.dev@gmail.com>
2021-09-05 03:32:31 +0900

    Add initial configs

---
+++ webpack.config.ts
@@ -0,0 +1,22 @@
+import { Configuration } from 'webpack'
+import * as path from 'path'
+
+const config: Configuration = {
+  mode: 'development',
+  entry: path.resolve('src', 'index.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  }
+}
+
+export default config
