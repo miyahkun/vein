607a8091b9a2a6edc7f69e46f573f672449b80fc
Keiji Miyazaki <miyah.dev@gmail.com>
2021-09-05 04:21:03 +0900

    Add some settings to webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,20 @@
-import { Configuration } from 'webpack';
-import * as path from 'path';
+import type { Configuration as WebpackConfiguration } from 'webpack';
+import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const config: Configuration = {
   mode: 'development',
   entry: path.resolve('src', 'index.ts'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    // clean: true
   },
   devtool: 'eval-source-map',
   module: {
@@ -19,7 +27,17 @@ const config: Configuration = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  devServer: {
+    static: 'dist',
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vein'
+    })
+  ]
 };
 
 export default config;
